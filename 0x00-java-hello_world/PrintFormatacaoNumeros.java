d6982cd860e9a8395d9c3da8ce3a61731fd52822
thiagosougo1 <thiagosg@ciandt.com>
2022-06-13 15:36:50 -0300

    Ex6

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -2,7 +2,7 @@ public class PrintFormatacaoNumeros {
     public static void main(String[] args) {
         float taxa = 0.2456f;
         float valor = 7654.321f;
-        System.out.printf("Valor: R$ %.2f \n", valor);
+        System.out.printf("Valor: R$ %.2f\n", valor);
         System.out.printf("Taxa: %.2f%% \n", taxa);
     }
 }
