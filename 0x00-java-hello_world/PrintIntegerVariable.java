cde96ec6e7f90213602981ba4cf1871027e26469
thiagosougo1 <thiagosg@ciandt.com>
2022-06-13 14:58:47 -0300

    Ex3

--- 0x00-java-hello_world/PrintIntegerVariable.java
+++ 0x00-java-hello_world/PrintIntegerVariable.java
@@ -2,6 +2,5 @@ public class PrintIntegerVariable {
     public static void main(String[] args) {
         int numero = 311;
         System.out.println("Avenida Andaló, " + numero);       
-        System.out.println();
     }
 }
