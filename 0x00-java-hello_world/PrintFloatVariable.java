b766d89ec483051cc38c0adbabf2ffa97e1b32fd
thiagosougo1 <thiagosg@ciandt.com>
2022-06-13 15:02:15 -0300

    Ex3

--- 0x00-java-hello_world/PrintFloatVariable.java
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -1,6 +1,6 @@
 public class PrintFloatVariable {
     public static void main(String[] args) {
-       float pi = (float) 3.142;
-        System.out.printf("Valor de pi: " + pi + " ");
+       float pi = (float) 3.14200;
+        System.out.printf("Valor de pi: %.2f ", pi);
     }
 }
