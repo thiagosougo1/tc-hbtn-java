fd058d0ad5c711f01141cd57df90777b9167f71b
thiagosougo1 <thiagosg@ciandt.com>
2022-06-13 15:19:44 -0300

    Ex5

--- 0x00-java-hello_world/PrintStringConcat.java
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -2,6 +2,7 @@ public class PrintStringConcat {
     public static void main(String[] args) {
         String texto1 = "Holberton";
         String texto2 = " School";
-        System.out.println("Bem vindo a " + texto1 + texto2);
+        String concat = texto1 + texto2;
+        System.out.println("Bem vindo a " + concat + "!");
     }
 }
