166be9be8ed89d74dbac6a5058011261ac4a9283
thiagosougo1 <107259942+thiagosougo1@users.noreply.github.com>
2022-06-13 15:49:39 -0300

    Update PrintFormatacaoNumeros.java

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -5,7 +5,7 @@ public class PrintFormatacaoNumeros {
         float taxa = 0.2456f;
         float valor = 7654.321f;
         DecimalFormat df = new DecimalFormat("###,###,###,###,###.00");
-        System.out.println("Valor: R$" + df.format(valor));
+        System.out.println("Valor: R$ " + df.format(valor));
         System.out.printf("Taxa: %.2f%% \n", taxa);
     }
 }
