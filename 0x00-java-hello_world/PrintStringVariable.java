1cda78a3115e96e4992cb1e0906e313cabfa00f8
thiagosougo1 <thiagosg@ciandt.com>
2022-06-13 15:10:50 -0300

    Ex4

---
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -0,0 +1,9 @@
+public class PrintStringVariable {
+    public static void main(String[] args) {
+        String texto = "Holberton School";
+        System.out.println(texto);
+        System.out.println(texto);
+        System.out.println(texto.substring(0, 10));
+
+    }
+}
