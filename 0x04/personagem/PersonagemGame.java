4850033494f1b76ab8c07611d673f94657a1d817
thiagosougo1 <thiagosg@ciandt.com>
2022-06-15 19:15:11 -0300

    Ex - 0

---
+++ 0x04/personagem/PersonagemGame.java
@@ -0,0 +1,11 @@
+public class PersonagemGame {
+
+    String nome;
+    int saudeAtual;
+
+    public PersonagemGame() {
+        this.nome = nome;
+        this.saudeAtual = saudeAtual;
+    }
+    
+}
