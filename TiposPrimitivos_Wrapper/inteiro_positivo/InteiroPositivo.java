29dc299447e0d917a717d8958c54b6e4f4184503
thiagosougo1 <thiagosg@ciandt.com>
2022-06-15 15:39:03 -0300

    Ex0

--- TiposPrimitivos_Wrapper/inteiro_positivo/InteiroPositivo.java
+++ TiposPrimitivos_Wrapper/inteiro_positivo/InteiroPositivo.java
@@ -2,8 +2,32 @@ package TiposPrimitivos_Wrapper.inteiro_positivo;
 
 public class InteiroPositivo {
     private int valor;
+    String dado;
+
+    InteiroPositivo(int valor) {
+        this.valor = valor;
+    }
+
+    InteiroPositivo(String valor) {
+        this.valor = Integer.parseInt(valor);
+    }
+
+    public int getValor() {
+        return this.valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
 
     Boolean ehPrimo(){
+        if (valor <= 1)
         return false;
+
+        for (int i = 2; i < valor; i++){
+            if (valor % i == 0)
+                return false;
+        }
+        return true;
     }
 }
