674bdcd7b371d24377f956d5a551b7fc77a3db9c
thiagosougo1 <thiagosg@ciandt.com>
2022-06-16 10:15:37 -0300

    Ex 01

--- 0x09/agregacao/Empregado.java
+++ 0x09/agregacao/Empregado.java
@@ -27,6 +27,6 @@ public class Empregado {
         System.out.println("Nome: " + nome);
         System.out.println("Bairro: " + endereco.getBairro());
         System.out.println("Cidade: " + endereco.getCidade());
-        System.out.print("País: " + endereco.getPais());
+        System.out.print("Pais: " + endereco.getPais());
     }
 }
