0d966790bbc012e7a8f3a6a93b2b381661d0e462
thiagosougo1 <thiagosg@ciandt.com>
2022-06-20 18:09:57 -0300

    Ex 0

--- 0x10/plano_mordor/Personagem.java
+++ 0x10/plano_mordor/Personagem.java
@@ -1,3 +1,20 @@
 public class Personagem {
-    
+    int pontosDeFelicidade;
+
+    Personagem(int pontosDeFelicidade){
+        this.pontosDeFelicidade = pontosDeFelicidade;
+    }   
+
+    public Humor obterHumorAtual(){
+        return Humor;
+    }
+
+    public void comer(Comida[] comidas){
+
+    }
+
+    @Override
+    public string toString(){
+        return pontosDeFelicidade + " " + obterHumorAtual().getClass().getName(); 
+    }
 }
