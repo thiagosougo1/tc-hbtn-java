caf05ded4c79bb14fe33c25aa988af0b82bfb316
thiagosougo1 <thiagosg@ciandt.com>
2022-06-20 12:30:29 -0300

    Ex - 0

--- 0x10/plano_mordor/comida/Comida.java
+++ 0x10/plano_mordor/comida/Comida.java
@@ -1,8 +1,7 @@
-package comida;
 
 public abstract class Comida {
     protected int pontosDeFelicidade;
-    
+
     public Comida(int pontosDeFelicidade){
         this.pontosDeFelicidade = pontosDeFelicidade;
     }
