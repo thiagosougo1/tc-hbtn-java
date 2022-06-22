c2f3a36abdfb625618b4fcfc66a81e3a1014cbf8
thiagosougo1 <thiagosg@ciandt.com>
2022-06-22 10:20:01 -0300

    Ex 1

---
+++ 0x01/numeros/NumerosPares.java
@@ -0,0 +1,12 @@
+public class NumerosPares {
+    public static void main(String[] args) {
+        int i = 0;
+        while(i < 97){
+            if(i % 2 ==0 ){
+                System.out.print(i + ", ");
+            }
+            i++;
+        }
+        System.out.println("98");
+    }
+}
