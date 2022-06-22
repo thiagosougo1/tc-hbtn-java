7ca4f5660affc432c9f54639508030d672d3df4e
thiagosougo1 <thiagosg@ciandt.com>
2022-06-22 10:16:10 -0300

    Ex 0

---
+++ 0x01/numeros/Numeros0a99.java
@@ -0,0 +1,10 @@
+public class Numeros0a99 {
+    public static void main(String[] args) {
+        int i = 0;
+        while(i < 100){
+            System.out.print(i + ", ");
+            i++;
+        }
+        System.out.println();
+    }
+}
