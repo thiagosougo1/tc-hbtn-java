2aa60c9d32ca94fe6c7ab554daaaf19d8ecfa4f9
thiagosougo1 <thiagosg@ciandt.com>
2022-06-22 10:24:53 -0300

    Ex 2

---
+++ 0x01/maiusculo/Caractere.java
@@ -0,0 +1,12 @@
+package maiusculo;
+
+public class Caractere {
+
+    public static boolean EhMaiusculo(char c) {
+        if(c >= 'A' && c <= 'Z'){
+            return true;
+        } else
+        return false;
+    }
+    
+}
