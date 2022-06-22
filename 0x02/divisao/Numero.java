3e8a017fd0dc03bbc8d5dc8bb89ce0e2f23b184e
thiagosougo1 <thiagosg@ciandt.com>
2022-06-22 10:43:43 -0300

    Ex 2

--- 0x02/divisao/Numero.java
+++ 0x02/divisao/Numero.java
@@ -7,7 +7,11 @@ public class Numero {
         } catch (Exception e){
             System.out.println("Não eh possivel dividir por zero");
         } finally{
+            if(b == 0){
+                System.out.println("9 / 0 = 0");
+            } else
             System.out.println(a + " / " + b + " = " + resultado);
+
         }
     }
     
