51db17e08ee9002ef623a58dce18d0d48554c26a
thiagosougo1 <thiagosg@ciandt.com>
2022-06-24 11:12:47 -0300

    Ex 2

--- 0x03/conta_basica/Program.java
+++ 0x03/conta_basica/Program.java
@@ -1,5 +1,7 @@
 public class Program {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.FRENCH);
+
         try {
             ContaBancariaBasica conta1 = new ContaBancariaBasica("AAA", 50.0);
             System.out.printf("Conta %s - saldo: %.2f\n", conta1.getNumeracao(), conta1.getSaldo());
