92a47a1ca14366fe220cf896f548ec0610b258b8
thiagosougo1 <thiagosg@ciandt.com>
2022-06-24 11:10:58 -0300

    Ex 2

--- 0x03/conta_basica/exceptions/OperacaoInvalidaException.java
+++ 0x03/conta_basica/exceptions/OperacaoInvalidaException.java
@@ -1,4 +1,4 @@
-package conta_basica.exceptions;
+package exceptions;
 
 public class OperacaoInvalidaException extends Exception{
     public OperacaoInvalidaException(String mensagem) throws Exception{
