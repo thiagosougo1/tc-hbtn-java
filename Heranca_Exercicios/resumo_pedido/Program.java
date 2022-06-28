354ceeb5a7bd51d84cc0ecd8173f72354ca4c78c
thiagosougo1 <thiagosg@ciandt.com>
2022-06-28 10:44:24 -0300

    EX 1

--- Heranca_Exercicios/resumo_pedido/Program.java
+++ Heranca_Exercicios/resumo_pedido/Program.java
@@ -3,8 +3,6 @@ import java.util.Locale;
 import produtos.Dvd;
 import produtos.Livro;
 
-package RESUMO_PEDIDO;
-
 public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.FRENCH);
