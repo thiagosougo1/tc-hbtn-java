d66b201cc002aeff431fcd898ca4a9e89e996632
thiagosougo1 <thiagosg@ciandt.com>
2022-06-28 11:09:53 -0300

    Ex 1

--- Heranca_Exercicios/resumo_pedido/Pedido.java
+++ Heranca_Exercicios/resumo_pedido/Pedido.java
@@ -48,7 +48,7 @@ public class Pedido {
             System.out.println();
         }
         System.out.println("----------------------------");
-        System.out.printf("DESCONTO: %.2f\n", getPercentualDesconto());
+        System.out.printf("DESCONTO: %.2f\n", sumSemDesconto - calcularTotal());
         System.out.printf("TOTAL PRODUTOS: %.2f\n", sumSemDesconto);
         System.out.println("----------------------------");
         System.out.printf("TOTAL PEDIDO: %.2f\n", calcularTotal());
