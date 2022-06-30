0fb1ba8940d406b56f7105d32329b3bb1c0dfd2d
thiagosougo1 <thiagosg@ciandt.com>
2022-06-30 12:56:59 -0300

    EX 0

--- Set_Map/analisador_frase/AnalisadorFrase.java
+++ Set_Map/analisador_frase/AnalisadorFrase.java
@@ -11,13 +11,12 @@ public class AnalisadorFrase {
 
     public static TreeMap contagemPalavras(String frase){
         TreeMap<String, Integer> contPalavras = new TreeMap<String, Integer>();
-        String[] fraseSplit = frase.toLowerCase().replace(".", " ").replace(",", " ").replace("?", " ").replace("!", " ").split(" ");
+        String[] fraseSplit = frase.toLowerCase().replace(".", " ").replace("?", " ").replace("!", " ").split(" ");
         List<String> listPalavras = new ArrayList<>(List.of(fraseSplit));
 
         for (int i = 0; i < fraseSplit.length; i++) {
             contPalavras.put(fraseSplit[i], Collections.frequency(listPalavras, fraseSplit[i]));
         }
-        System.out.println();
         return contPalavras;
     }
 }
