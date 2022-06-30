f90c66c31e4ca08d2c8f4bae5b68834f9ea322e1
thiagosougo1 <thiagosg@ciandt.com>
2022-06-30 15:34:12 -0300

    EX 1

--- Set_Map/duplicados/NumerosDuplicados.java
+++ Set_Map/duplicados/NumerosDuplicados.java
@@ -1,18 +1,27 @@
 package duplicados;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class NumerosDuplicados {
     public static TreeSet buscar(int[] valores){
-        Set<Integer> ts = new HashSet<>();
+        List<Integer> valor = new ArrayList<>();
+        Set<Integer> ts = new TreeSet<>();
+
         for (int i = 0; i < valores.length; i++) {
-            ts.add(valores[i]);
-           // System.out.print("["+valores[i]+"] ");
+            valor.add(valores[i]);
+            
+            if(Collections.frequency(valor, valores[i]) > 1){
+                ts.add(valores[i]);
+            };
         }
-        System.out.println();
-    System.out.println(ts);
+
+        System.out.println(ts);
+
         return null;
     }
 }
