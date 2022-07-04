4ee9975be749a2b6dbbf6a9af86cdbff4f38bc40
thiagosougo1 <thiagosg@ciandt.com>
2022-07-04 12:43:30 -0300

    Ex 1

--- Enum/banco/Agencia.java
+++ Enum/banco/Agencia.java
@@ -1,5 +1,59 @@
-package banco;
 
+import java.util.*;
 public class Agencia {
-    
+
+    String nome ;
+
+    ArrayList<Cliente> clientes ;
+
+    public Agencia(String nome) {
+        this.nome = nome;
+        clientes = new ArrayList<Cliente>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public ArrayList<Cliente> getClientes() {
+        return clientes;
+    }
+
+
+    public boolean novoCliente(String nome, double valorTransacao){
+       Cliente resultado =  buscarCliente(nome);
+
+       if (resultado == null){
+           Cliente n1 = new Cliente(nome , valorTransacao);
+           clientes.add(n1);
+           return true ;
+       }
+
+        return false;
+    }
+
+
+    public boolean adicionarTransacaoCliente(String nome, double transacao){
+        Cliente resultado =  buscarCliente(nome);
+
+        if (resultado != null){
+            resultado.adicionarTransacao(transacao);
+            return true ;
+        }
+
+        return false;
+
+
+    }
+
+
+    public Cliente buscarCliente(String nomeCliente){
+        for (Cliente nome: clientes) {
+            if (nome.getNome().equals(nomeCliente)){
+                return nome ;
+            }
+
+        }
+        return null;
+    }
 }
--- Enum/banco/Cliente.java
+++ Enum/banco/Cliente.java
@@ -1,5 +1,29 @@
-package banco;
 
+import java.util.*;
 public class Cliente {
-    
+    private String nome;
+    private ArrayList<Double> transacoes;
+
+    public Cliente() {
+
+    }
+
+    public Cliente(String nome, double transacao) {
+        this.nome = nome;
+        this.transacoes = new ArrayList<>();
+    }
+
+
+    public void adicionarTransacao(double valorDaTransacao) {
+        transacoes.add(valorDaTransacao);
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public ArrayList<Double> getTransacoes() {
+        return transacoes;
+    }
 }
