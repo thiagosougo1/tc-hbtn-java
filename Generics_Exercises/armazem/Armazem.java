a0a35d632248b77310cb1873bc1aa7c246e9d181
thiagosougo1 <thiagosg@ciandt.com>
2022-07-06 22:57:26 -0300

    EX 1

--- Generics_Exercises/armazem/Armazem.java
+++ Generics_Exercises/armazem/Armazem.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Armazem<T> extends Armazenavel {
     Map<String, T> itensArmazenados = new TreeMap<String, T>();
 
+    @SuppressWarnings("unchecked")
     @Override
     public void adicionarAoInventario(String nome, Object valor){
         itensArmazenados.put(nome, (T) valor);
