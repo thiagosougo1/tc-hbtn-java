90feacdb53e15f362aaaa1f0e16218025582117b
thiagosougo1 <thiagosg@ciandt.com>
2022-07-06 23:20:40 -0300

    EX 1

--- Generics_Exercises/armazem_especifico/Program.java
+++ Generics_Exercises/armazem_especifico/Program.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 public class Program {
        public static void main(String[] args) {
+        Locale.setDefault(Locale.FRENCH);
+
             Comida comida1 = new Comida("arroz", 100, 15.9);
             Comida comida2 = new Comida("feijao", 80, 23.0);
             Comida comida3 = new Comida("pacoca", 165, 7.5);
