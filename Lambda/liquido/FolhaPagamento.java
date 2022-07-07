daa357343faf22f557a09e42060347bb5350dab2
thiagosougo1 <thiagosg@ciandt.com>
2022-07-07 11:53:18 -0300

    EX 0

---
+++ Lambda/liquido/FolhaPagamento.java
@@ -0,0 +1,10 @@
+public class FolhaPagamento implements CalculadorDeSalario{
+    public static double calcularLiquido(Funcionario funcionario, CalculadorDeSalario calculador){
+        return 0;
+    }
+
+    @Override
+    public double calcular(double valor) {
+        return 0;
+    }
+}
---
+++ Lambda/liquido/Funcionario.java
@@ -0,0 +1,16 @@
+public class Funcionario {
+    private double salario;
+
+    public Funcionario(double salario) {
+        setSalario(salario);
+    }
+
+    public double getSalario() {
+        return this.salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+}
