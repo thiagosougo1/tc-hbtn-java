e2b2eb6406c2a999ba3c4866e69c678e88ffc52f
thiagosougo1 <thiagosg@ciandt.com>
2022-07-11 16:14:53 -0300

    EX 0

--- Lambda_Exercicios/consulta_produtos/ConsultaProdutos.java
+++ Lambda_Exercicios/consulta_produtos/ConsultaProdutos.java
@@ -3,9 +3,13 @@ import java.util.*;
 public class ConsultaProdutos {
     public static List<Produto> filtrar(List<Produto> produtos, CriterioFiltro criterioFiltro){
         List<Produto> lista = new ArrayList<>();
+
         produtos.forEach(produto -> {
-            lista.add(produto);
+            if(criterioFiltro.testar(produto)){
+                lista.add(produto);
+            }
         });
+
         return lista;
     }
 }
--- Lambda_Exercicios/consulta_produtos/CriterioFiltro.java
+++ Lambda_Exercicios/consulta_produtos/CriterioFiltro.java
@@ -1,3 +1,4 @@
 public interface CriterioFiltro {
     public boolean testar(Produto produto);
+
 }
