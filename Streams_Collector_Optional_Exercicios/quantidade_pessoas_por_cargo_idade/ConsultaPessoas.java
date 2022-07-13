a54a59e6ccb09e5adeedc57440003197ab727d3a
thiagosougo1 <thiagosg@ciandt.com>
2022-07-13 18:42:45 -0300

    EX 0

---
+++ Streams_Collector_Optional_Exercicios/quantidade_pessoas_por_cargo_idade/ConsultaPessoas.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoas){
+        TreeMap<String, TreeSet<Pessoa>> mapPessoas = pessoas.stream().collect(
+            Collectors.groupingBy(Pessoa::getCargo, () -> new TreeMap<>(Comparator.reverseOrder()), 
+            Collectors.toCollection(TreeSet::new))
+        );
+        return mapPessoas;
+    }
+
+    public static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> listaPessoas){
+        return listaPessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.counting()));
+    }
+
+    public static Map<String, Map<Integer, Long>> obterContagemPessoasPorCargoEIdade(List<Pessoa> listaPessoas){
+        return listaPessoas.stream().collect(
+            Collectors.groupingBy(Pessoa::getCargo, 
+            Collectors.groupingBy(Pessoa::getIdade, Collectors.counting())
+        ));
+    }
+}
