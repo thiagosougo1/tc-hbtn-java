b53e96f4f1ff9ab36467db016cbe8c72e1014cb8
thiagosougo1 <thiagosg@ciandt.com>
2022-07-13 18:30:15 -0300

    EX 0

--- Stream_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa/ConsultaPessoas.java
+++ Stream_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa/ConsultaPessoas.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class ConsultaPessoas {
-    public TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoas){
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoas){
         TreeMap<String, TreeSet<Pessoa>> mapPessoas = pessoas.stream().collect(
             Collectors.groupingBy(Pessoa::getCargo, () -> new TreeMap<>(Comparator.reverseOrder()), 
             Collectors.toCollection(TreeSet::new))
