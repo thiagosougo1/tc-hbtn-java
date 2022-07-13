3852b28ccccad9c37da57988612b54b94585baa9
thiagosougo1 <thiagosg@ciandt.com>
2022-07-13 12:13:04 -0300

    EX 0

---
+++ Streams_Collectors_Optional/pedidos_eletronico/Pedido.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Pedido {
+    private int codigo;
+    private List<Produto> produto;
+    private Cliente cliente;
+
+    public Pedido(int codigo, List<Produto> produto, Cliente cliente) {
+        setCodigo(codigo);
+        setProduto(produto);
+        setCliente(cliente);
+    }
+
+    public int getCodigo() {
+        return this.codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public List<Produto> getProdutos() {
+        return this.produto;
+    }
+
+    public void setProduto(List<Produto> produto) {
+        this.produto = produto;
+    } 
+
+    public Cliente getCliente() {
+        return this.cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    @Override 
+    public String toString(){
+        return String.format("[%d] $s", getCodigo(), getCliente().getNome());
+    }
+}
