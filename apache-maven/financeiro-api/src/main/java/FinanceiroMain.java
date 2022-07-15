25df5addd9043cde025ffa5a53390a6a69cf0081
thiagosougo1 <thiagosg@ciandt.com>
2022-07-15 13:31:33 -0300

    Ex 0

---
+++ apache-maven/financeiro-api/src/main/java/FinanceiroMain.java
@@ -0,0 +1,12 @@
+
+/**
+ * Hello world!
+ *
+ */
+public class FinanceiroMain 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Criando um projeto Java MAven." );
+    }
+}
